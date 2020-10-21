a2a7df6de10ad755ab956b188cda135bc96a7d66
Mauricio Blum <mauriciocblum@gmail.com>
2020-10-21 18:12:07 +0100

    refactor to catalogItem

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import api from '../services/api';
-import { addProductToCart } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
+import CatalogItem from './CatalogItem';
 
 const Catalog: React.FC = () => {
-  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -14,25 +13,12 @@ const Catalog: React.FC = () => {
     });
   }, []);
 
-  const handleAddProductToCart = useCallback(
-    (product: IProduct) => {
-      dispatch(addProductToCart(product));
-    },
-    [dispatch]
-  );
-
   return (
     <main>
       <h1>Catalog</h1>
 
       {catalog.map((product) => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {' - '}
-          <span>{product.price}</span>{' '}
-          <button onClick={() => handleAddProductToCart(product)} type="button">
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
   );
